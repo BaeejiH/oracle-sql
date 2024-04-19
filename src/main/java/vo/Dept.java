b99a63d20fc6df13a287eacf5b228fe041c30c4b
BaeejiH <bjihun0511@gmail.com>
2024-04-19 16:45:13 +0900

    [배지훈]접근 제한자 사용하여 VO정보은닉 후 캡슐화.-> Generate getter and setter 변경.

--- src/main/java/vo/Dept.java
+++ src/main/java/vo/Dept.java
@@ -3,7 +3,28 @@ package vo;
 //Vo(vlaue Object) > DTO(Data Transfer Object) , Domain(속성 값의 범위 , 테이블 속성의 범위)
 
 public class Dept {
-	public int deptNo;
-	public String dname;
-	public String loc;
+	
+	private int deptNo;
+	private String dname;
+	private String loc;
+	
+	public int getDeptNo() {
+		return deptNo;
+	}
+	public void setDeptNo(int deptNo) {
+		this.deptNo = deptNo;
+	}
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	
 }
